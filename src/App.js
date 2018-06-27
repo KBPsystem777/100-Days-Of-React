1bcd3247e9d26b525945d733c81ccc8a73948043
kbpsystem777 <koleen.bp@outlook.com>
2018-06-27 08:35:17 +0800

    broke things down...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import React, { Component } from 'react';
-import './timeline.css';
+import 'w3-css/w3.css';
+
+
+
+class Event extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="item">
+          <div className="avatar">
+            <img
+              alt="doug"
+              src="http://www.croop.cl/UI/twitter/images/doug.jpg"
+            />
+          </div>
+          <span>{this.props.time}</span>
+          <p>{this.props.status}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const SearchButton = () => <div className="fa fa-search searchIcon"></div>;
+
+const InputForm = () => <div>
+  <input
+    type="text"
+    className="searchInput"
+    placeholder="Search..." />
+  </div>
 
 class Timeline extends React.Component {
   render() {
@@ -12,23 +42,13 @@ class Timeline extends React.Component {
               <div className="dashButtom"></div>
               <div className="circle"></div>
             </div>
-            <span className="title">Timeline</span>
-            <input
-              type="text"
-              className="searchInput"
-              placeholder="Search..." />
-            <div className="fa fa-search searchIcon"></div>
-          </div>
-          <div className="content">
-            <div className="line"></div>
-            <div className="item">
-              <div className="avatar">
-                <img
-                  alt="doug"
-                  src="http://www.croop.cl/UI/twitter/images/doug.jpg"
-                />
-              </div>
-            </div>
+            <span className="w3-large">Timeline</span>
+            <SearchButton />
+            <InputForm />
+            <Event status= "Ate Lunch" time = "An hour ago"/>
+            <Event status="Travelling back home" time="3 Hours ago"/>
+            <Event status="Meeting with the gangsters!" time="5 Hours ago"/>
+            <Event status="Will Sleep now..." time="A day ago"/>
           </div>
         </div>
       </div>
